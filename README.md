a0ebc5892e8bfc60a216de107c6296da07f16ab0
Stefan Peters <Stefan.Peters@gbv.de>
2019-08-19 10:44:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+This is a test repository to demonstrate the bug in [wikibase-edit](https://github.com/maxlath/wikibase-edit) (or potentially Wikibase itself) that is described here: https://github.com/maxlath/wikibase-edit/issues/34
+
+Apparently, if you use OAuth authentication, only `claim.update` fails to authenticate while other actions work. This repository demostrates that behavior by creating a claim, trying to update it, and deleting it again. You'll see that creating and deleting works, but updating does not.
+
+## How to run
+
+First, clone the repository and install the dependency (which is wikibase-edit):
+```bash
+git clone https://github.com/stefandesu/wikibase-edit-claim-update-example.git
+cd wikibase-edit-claim-update-example
+npm i
+```
+
+Then, create a `config.json` file with your OAuth credentials like this:
+```json
+{
+  "oauth": {
+    "token": "...",
+    "token_secret": "...",
+    "consumer_key": "...",
+    "consumer_secret": "..."
+  }
+}
+```
+(Note that this assume you already have a registered OAuth consumer and the token+token_secret for an authorized user for that consumer.)
+
+Optional: In `index.js`, you can adjust which entity is going to be used for the test and which claim should be created. By default, [Wikidata Sandbox](https://www.wikidata.org/wiki/Q4115189) is used with a claim for [Basisklassifikation](https://www.wikidata.org/wiki/Property:P5748).
+
+Finally, run the script and see the error:
+```bash
+node index.js
+```
+
+Here's what the output should look like:
+
+![](screenshot.png)
+
+If, for some reason, deleting the created claim was not successful, please remove the claim manually so that the test will work in the future as well.
---
+++ index.js
@@ -0,0 +1,63 @@
+// Adjust config.json with OAuth credentials
+const wdEdit = require("wikidata-edit")(require("./config.json"))
+
+// Adjust entity for testing if necessary (default is Wikidata Sandbox)
+const entity = "Q4115189"
+// Adjust claim that will be created/updated/removed for testing if necessary
+// (default is "Basisklassifikation")
+const claimProperty = "P5748"
+const claimValueCreate = "10"
+const claimValueUpdate = "20"
+
+runTest()
+
+async function runTest() {
+
+  console.log("- Attempting to create claim...")
+
+  // 1. Create claim on entity
+  let guid
+  try {
+    const addClaimResult = await wdEdit.claim.add(entity, claimProperty, claimValueCreate)
+    guid = addClaimResult && addClaimResult.claim.id
+  } catch(error) {
+    console.error(error)
+  }
+
+  if (!guid) {
+    // Doesn't make sense to continue
+    console.error("Error when creating claim, please check OAuth credentials.")
+    return
+  }
+
+  console.log("- Created claim with GUID:", guid)
+  console.log()
+
+  console.log("- Attempting to update claim (will fail)...")
+
+  // 2. Update claim that was just created
+  try {
+    const updateClaimResult = await wdEdit.claim.update(guid, claimValueUpdate)
+    if (!updateClaimResult.success) {
+      throw new Error("Updating claim was apparently not successful.")
+    }
+    console.log("- Updating claim was successful.")
+  } catch(error) {
+    console.error(error)
+    console.error("- Updating claim failed with above error.")
+  }
+
+  console.log()
+
+  console.log("- Attempting to delete claim (for cleanup)...")
+
+  // 3. Delete claim again
+  try {
+    await wdEdit.claim.remove(guid)
+    console.log("- Deleting claim was successful.")
+  } catch(error) {
+    console.error(error)
+    console.error("- Deleting claim failed with above error (please delete claim manually).")
+  }
+
+}
